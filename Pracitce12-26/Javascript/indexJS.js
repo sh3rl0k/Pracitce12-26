55bb763cdd02d80b235f58fcdabb18c27a911754
Jackson, William G <wgjackson@student.rtc.edu>
2020-02-03 10:51:51 -0800

    My homepage I have been working on. It still needs work.

---
+++ Pracitce12-26/Javascript/indexJS.js
@@ -0,0 +1,16 @@
+﻿var Fname = document.getElementById("fname").value;
+var Lname = document.getElementById("lname").value;
+var Company = document.getElementById("company").value;
+var Email = document.getElementById("email").value;
+
+//function to check if the text fields are empty
+document.getElementById("btnform1").onclick = function () {
+    var x = document.forms["myForm"]["fname", "lname","company","email"].value;
+    if (x == "") {
+        alert("All fields must be filled out");
+        return false;
+    }
+}
+//document.getElementById("btnform1").onclick = function () {
+    
+//}
